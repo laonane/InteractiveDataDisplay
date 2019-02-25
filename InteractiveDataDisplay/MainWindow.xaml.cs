8a4dbaff881d12d19ce300f82e8613ea1159ced5
HuaiAnGG <971375353@qq.com>
2019-02-25 18:06:22 +0800

    实现窗口跟随移动。

--- InteractiveDataDisplay/MainWindow.xaml.cs
+++ InteractiveDataDisplay/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Research.DynamicDataDisplay;
 using Microsoft.Research.DynamicDataDisplay.DataSources;
 using System;
+using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
@@ -21,6 +22,7 @@ namespace InteractiveDataDisplay
         private DispatcherTimer dispatcherTimer = new DispatcherTimer();//创建一个定时器
         private int currentSecond = 0;
 
+        bool rock = false;//标志是否滚屏
         public MainWindow()
         {
             InitializeComponent();
@@ -28,14 +30,19 @@ namespace InteractiveDataDisplay
         
         private void Window_Loaded_01(object sender, RoutedEventArgs e)
         {
-            plotter.AddLineGraph(dataSource, Colors.Green, 2, "Percentage");
-            //plotter.LegendVisible = false;
+            plotter.AddLineGraph(dataSource, Colors.Green, 3, "Percentage");
+            plotter.LegendVisible = true;
             dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
             dispatcherTimer.Tick += Timer_Tick_CPU;
             dispatcherTimer.IsEnabled = true;
             plotter.Viewport.FitToView();
         }
-        
+
+        int xaxis = 0;
+        int yaxis = 0;
+        int group = 20;//默认组距
+
+        Queue q = new Queue();
         private void Timer_Tick_CPU(object sender, EventArgs e)
         {
             performanceCounter.CategoryName = "Processor";
@@ -48,7 +55,48 @@ namespace InteractiveDataDisplay
             Point point = new Point(x, y);
             dataSource.AppendAsync(base.Dispatcher, point);
 
+            if (rock)
+            {
+                if (q.Count < group)
+                {
+                    q.Enqueue((int)y);//入队
+                    yaxis = 0;
+                    foreach (int c in q)
+                        if (c > yaxis)
+                            yaxis = c;
+                }
+                else
+                {
+                    q.Dequeue();//出队
+                    q.Enqueue((int)y);//入队
+                    yaxis = 0;
+                    foreach (int c in q)
+                        if (c > yaxis)
+                            yaxis = c;
+                }
+
+                if (currentSecond - group > 0)
+                    xaxis = currentSecond - group;
+                else
+                    xaxis = 0;
+
+                Debug.Write("{0}\n", yaxis.ToString());
+                plotter.Viewport.Visible = new System.Windows.Rect(xaxis, 0, group, yaxis);//主要注意这里一行
+            }
+
             currentSecond++;
+        }
+
+        private void Btn_Rock_click_01(object sender, EventArgs e)
+        {
+            if (rock)
+            {
+                rock = false;
+            }
+            else
+            {
+                rock = true;
+            }
         }
     }
 }
